ba57c2e9bc9fbc216f05cfc2747e2a1c558924a7
Viktar Karpach <vkarpach@gmail.com>
2017-08-03 23:07:01 -0500

    Add menu interactions

--- SplitButtonDemo/ExampleWindow.xaml.cs
+++ SplitButtonDemo/ExampleWindow.xaml.cs
@@ -1,29 +1,36 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 
 namespace SplitButtonDemo
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for WindowExample.xaml
     /// </summary>
 
-    public partial class Window1 : System.Windows.Window
+    public partial class WindowExample : System.Windows.Window
     {
 
-        public Window1()
+        public WindowExample()
         {
             InitializeComponent();
         }
 
+        private void MenuItem_Click(Object sender, RoutedEventArgs e)
+        {
+            var menu = e.OriginalSource as MenuItem;
+            menu.Background = Brushes.Yellow;            
+        }
+
+        private void SplitButton_Click(Object sender, RoutedEventArgs e)
+        {
+            var menu = e.OriginalSource as MenuItem;
+            foreach (MenuItem item in menu.Items)
+            {
+                item.Background = Brushes.Transparent;
+            }
+        }
     }
 }
